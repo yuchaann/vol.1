e99c4323d04a7bc19003e3ae294bc622337c5296
yuchaann <ski1225h15@gmail.com>
2023-02-05 12:52:31 +0900

    ki

--- vol1.py
+++ vol1.py
@@ -11,32 +11,32 @@ class Player:
     def update(self,dx,dy):
         self.x=(self.x+dx)
         self.y=(self.y+dy)
+    def draw(self):
+        pyxel.circ(self.x,self.y,2,7)
 class Enemy:
     def __init__(self,x,y):
         self.x=x
         self.y=y
-    def update(self):
+    def draw(self):
         pyxel.rect(self.x,self.y, 2, 2,7)
 class App:
     def __init__(self):
         pyxel.init(200, 150)
         pyxel.cls(0)
+        self.player = Player(100,100)
         self.enemy = Enemy(170,40)
-        pyxel.run(self.update, self.draw)
+        pyxel.run(self.draw, self.update)
     def update(self):
-        if pyxel.btn(pyxel.KEY2):
-            dx=1
-            dy=1
-        if pyxel.btn(pyxel.KEY1):
-            dx=-1
-            dy=1
-        if pyxel.btn(pyxel.KEY5):
-            dx=1
-            dy=-1
-        if pyxel.btn(pyxel.KEY4):
-            dx=-1
-            dy=-1
-        self.enemy.update(dx,dy)
+        if pyxel.btn(pyxel.KEY_2):
+             self.player.update(1,1)
+        if pyxel.btn(pyxel.KEY_1):
+             self.player.update(-1,1)
+        if pyxel.btn(pyxel.KEY_5):
+             self.player.update(1,-1)
+        if pyxel.btn(pyxel.KEY_4):
+             self.player.update(-1,-1)
+        
     def draw(self):
-        pass
+        self.player.draw()
+        self.enemy.draw()
 App()
